95017539ffd6ed77836df6e0ac84beb21caad165
Charlie Gaines <beaugaines@gmail.com>
2013-12-03 11:18:32 -0500

    vergin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewBillOyst::Application.config.secret_key_base = 'b6c8e0314acf909c16cadcedc384b4d4a8ce2d517c5b9f6148ef126283c5a4ae262ab29e841d01c510d237711386dbdbf6d57f57014d0ecd9069e5721b0d07ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewBillOyst::Application.config.session_store :cookie_store, key: '_new_bill_oyst_session'
