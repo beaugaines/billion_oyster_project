d3fa412063372752934a15d162ad1765bbf4c6b0
Charlie Gaines <beaugaines@gmail.com>
2013-12-03 16:08:27 -0500

    welcome layout and user accounts links

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 
   scope :first, -> { order('created_at DESC').first }
   scope :last, -> { order('created_at DESC').last }
