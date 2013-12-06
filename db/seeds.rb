0101af531b8029b2433267b907fafb587ae58155
Charlie Gaines <beaugaines@gmail.com>
2013-12-06 00:08:38 -0500

    seed file work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).pu
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+u = User.new(email: 'user@bop.com', password: ENV['USER_PASSWORD'], password_confirmation: ENV['USER_PASSWORD'])
+a = User.new(email: 'admin@bop.com', password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'])
+
+%w(u a).each do |account|
+  account.skip_confirmation!
+  account.save
+end
+
+
