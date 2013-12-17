e781cd642e0839e0aedb9dca293f60a1dc42f833
Charlie Gaines <beaugaines@gmail.com>
2013-12-17 12:03:06 -0500

    post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210034809) do
+ActiveRecord::Schema.define(version: 20131217162635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20131210034809) do
     t.datetime "updated_at"
     t.string   "street_address"
     t.string   "full_address"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "caption"
+    t.uuid     "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
