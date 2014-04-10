c53d09f217d09b480de6253d0c794b7d95b22f74
Charlie Gaines <beaugaines@gmail.com>
2014-04-10 17:38:09 -0400

    student observation spec

---
+++ spec/features/student_observation_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'Student observation spec', %q{
+  As a student
+  I want to be able to add observation data
+  From my mobile phone
+  So we can collect useful data for our project
+} do
+
+  before do
+    @user = create(:user)
+    @account = create(:account)
+    @user.update_attribute(:account, @account)
+  end
+
+  scenario 'student' do
+    login(@admin)
+    click_link 'Manage Accounts'
+    click_link 'Add new account'
+    fill_in 'Name', with: 'Harbor School'
+    fill_in 'City', with: 'New York'
+    select 'moderator@bop.com', from: 'Account Moderator'
+    click_button 'Create Account'
+    expect(Account.first.name).to eql('Harbor School')
+  end
+
+end
